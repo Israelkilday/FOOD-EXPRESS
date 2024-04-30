d0e2b9a53939d265461cab5267b3a0e392222d79
Israel Kilday <israel.kilday@yahoo.com.br>
2024-04-30 20:32:18 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6"></div>
+      <Search />
+    </>
+  );
 };
 
 export default Home;
