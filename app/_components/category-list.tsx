15feb4f82eb64d0bdb67342f664d1e96ac7477d2
Israel Kilday <israel.kilday@yahoo.com.br>
2024-05-01 00:08:15 -0300

    feat: add banner promo homepage

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -5,8 +5,7 @@ const CategoryList = async () => {
   const categories = await db.category.findMany({});
 
   return (
-    // <div className="grid grid-cols-2 gap-3">
-    <div className="flex overflow-x-scroll">
+    <div className="grid grid-cols-2 gap-3">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -6,11 +7,25 @@ const Home = () => {
   return (
     <>
       <Header />
+
       <div className="px-5 py-6">
         <Search />
       </div>
+
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
