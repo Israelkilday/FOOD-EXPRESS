5ac311e5f7ad2fb8d9e553108053df6207682467
Israel Kilday <israel.kilday@yahoo.com.br>
2024-04-30 21:56:57 -0300

    feat: add category item and list component

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -1,5 +1,17 @@
-const CategoryList = () => {
-  return <h1>teste</h1>;
+import { db } from "../_lib/prisma";
+import CategoryItem from "./category-item";
+
+const CategoryList = async () => {
+  const categories = await db.category.findMany({});
+
+  return (
+    // <div className="grid grid-cols-2 gap-3">
+    <div className="flex overflow-x-scroll">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
 };
 
 export default CategoryList;
