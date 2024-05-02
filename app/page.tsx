e1721152c8ba62ea3757f769166a7cd04a98be85
Israel Kilday <israel.kilday@yahoo.com.br>
2024-05-01 22:42:58 -0300

    feat: add promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -6,6 +5,7 @@ import ProductList from "./_components/product-list";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
+import Promobanner from "./_components/promo-banner";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -37,14 +37,9 @@ const Home = async () => {
       </div>
 
       <div className="px-5 pt-6">
-        <Image
+        <Promobanner
           src="/promo-banner-01.png"
           alt="Até 30% de desconto em pizzas"
-          width={0}
-          height={0}
-          className="h-auto w-full object-contain"
-          sizes="100vw"
-          quality={100}
         />
       </div>
 
@@ -60,6 +55,13 @@ const Home = async () => {
           </Button>
         </div>
         <ProductList products={products} />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Promobanner
+          src="/promo-banner-02.png"
+          alt="A partir de R$17,90 em lanches "
+        />
       </div>
     </>
   );
