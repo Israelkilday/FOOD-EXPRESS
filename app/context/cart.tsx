e1b9f338eca52ff3501cf008ef97a4a41bb66b65
Israel Kilday <israel.kilday@yahoo.com.br>
2024-05-10 19:49:51 -0300

    feat: add cart banner

--- app/context/cart.tsx
+++ app/context/cart.tsx
@@ -23,6 +23,7 @@ interface ICartContext {
   subTotalPrice: number;
   totalPrice: number;
   totalDiscount: number;
+  totalQuantity: number;
   addProductToCart: ({
     product,
     quantity,
@@ -50,6 +51,7 @@ export const CartContext = createContext<ICartContext>({
   subTotalPrice: 0,
   totalPrice: 0,
   totalDiscount: 0,
+  totalQuantity: 0,
   addProductToCart: () => {},
   decreaseProductQuantity: () => {},
   increaseProductQuantity: () => {},
@@ -66,12 +68,23 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   }, [products]);
 
   const totalPrice = useMemo(() => {
+    return (
+      products.reduce((acc, product) => {
+        return acc + calculateProductTotalPrice(product) * product.quantity;
+      }, 0) + Number(products?.[0]?.restaurant?.deliveryFree)
+    );
+  }, [products]);
+
+  const totalQuantity = useMemo(() => {
     return products.reduce((acc, product) => {
-      return acc + calculateProductTotalPrice(product) * product.quantity;
+      return acc + product.quantity;
     }, 0);
   }, [products]);
 
-  const totalDiscount = subTotalPrice - totalPrice;
+  const totalDiscount =
+    subTotalPrice -
+    totalPrice +
+    Number(products?.[0]?.restaurant?.deliveryFree);
 
   const decreaseProductQuantity = (productId: string) => {
     return setProducts((prev) =>
@@ -164,6 +177,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         subTotalPrice,
         totalPrice,
         totalDiscount,
+        totalQuantity,
         addProductToCart,
         decreaseProductQuantity,
         increaseProductQuantity,
--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -1,10 +1,11 @@
 import { db } from "@/app/_lib/prisma";
 import { notFound } from "next/navigation";
-import RestaurantImage from "../_componentes/restaurant-image";
+import RestaurantImage from "./_componentes/restaurant-image";
 import Image from "next/image";
 import { StarIcon } from "lucide-react";
 import DeliveryInfo from "@/app/_components/delivery-info";
 import ProductList from "@/app/_components/product-list";
+import CartBanner from "./_componentes/cart-banner";
 
 interface RestaurantPageProps {
   params: {
@@ -108,6 +109,8 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
           <ProductList products={category.products} />
         </div>
       ))}
+
+      <CartBanner restaurant={restaurant} />
     </div>
   );
 };
