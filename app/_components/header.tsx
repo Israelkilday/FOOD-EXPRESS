a216ea37841cb709a5d97bde3620e5a9d61116a2
Israel Kilday <israel.kilday@yahoo.com.br>
2024-05-11 16:59:26 -0300

    feat: add initial menu sheet

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,12 +2,32 @@
 
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import {
+  HeartIcon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  ScrollText,
+} from "lucide-react";
 import Link from "next/link";
-import { Sheet, SheetTrigger } from "./ui/sheet";
-// import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Separator } from "./ui/separator";
 
 const Header = () => {
+  const { data } = useSession();
+
+  const handleSignOutClick = () => signOut();
+  const handleSignInClick = () => signIn();
+
   return (
     <div className="flex justify-between px-5 pt-6">
       <div className="relative h-[30px] w-[100px]">
@@ -31,6 +51,94 @@ const Header = () => {
             <MenuIcon />
           </Button>
         </SheetTrigger>
+
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle className="text-left">Menu</SheetTitle>
+          </SheetHeader>
+
+          {data?.user ? (
+            <>
+              <div className="flex justify-between pt-6">
+                <div className="flex items-center gap-3 ">
+                  <Avatar>
+                    <AvatarImage
+                      src={data?.user?.image as string | undefined}
+                    />
+                    <AvatarFallback>
+                      {data?.user?.name?.split(" ")[0][0]}
+                      {data?.user?.name?.split(" ")[1][0]}
+                    </AvatarFallback>
+                  </Avatar>
+
+                  <div>
+                    <h3 className="font-semibold">{data?.user?.name}</h3>
+                    <span className="block text-xs text-muted-foreground">
+                      {data?.user?.email}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex items-center justify-between pt-10">
+                <h2 className="font-semibold">Olá, Faça seu Login!</h2>
+                <Button size="icon" onClick={handleSignInClick}>
+                  <LogInIcon />
+                </Button>
+              </div>
+            </>
+          )}
+
+          <div className="py-6">
+            <Separator />
+          </div>
+
+          <div space-y-2>
+            <Button
+              variant="ghost"
+              className="font- w-full justify-start space-x-3 rounded-full text-sm"
+            >
+              <HomeIcon size={16} />
+              <span className="block">Início</span>
+            </Button>
+
+            {data?.user && (
+              <>
+                <Button
+                  variant="ghost"
+                  className="font- w-full justify-start space-x-3 rounded-full text-sm"
+                >
+                  <ScrollText size={16} />
+                  <span className="block">Meus Pedidos</span>
+                </Button>
+
+                <Button
+                  variant="ghost"
+                  className="font- w-full justify-start space-x-3 rounded-full text-sm"
+                >
+                  <HeartIcon size={16} />
+                  <span className="block">Retaurantes Favoritos</span>
+                </Button>
+              </>
+            )}
+          </div>
+
+          <div className="py-6">
+            <Separator />
+          </div>
+
+          {data?.user && (
+            <Button
+              variant="ghost"
+              className="font- w-full justify-start space-x-3 rounded-full text-sm"
+            >
+              <LogOutIcon size={16} onClick={handleSignOutClick} />
+              <span className="block">Sair da Conta</span>
+            </Button>
+          )}
+        </SheetContent>
       </Sheet>
     </div>
   );
