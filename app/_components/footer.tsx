7fd8f5574fff8c816020db252b372aa6f986e61a
Israel Kilday <israel.kilday@yahoo.com.br>
2024-08-16 11:24:12 -0300

    refactor: update text in footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -147,9 +147,9 @@ const Footer = () => {
       <div className="flex flex-col bg-primary px-5 pb-5 pt-0 md:px-24">
         <Separator className="mb-6" />
         <div className="flex justify-between lg:flex-row">
-          <div className="flex flex-col text-start text-sm text-white md:flex-row lg:flex-row lg:text-base">
+          <div className="flex flex-col text-start text-sm text-white lg:gap-5 lg:text-base">
             <span className="mr-1">© 2024 FOOD EXPRESS</span>
-            <p>- Todos os direitos reservados </p>
+            <p>Todos os direitos reservados </p>
 
             <p className="text-sm font-bold text-white md:text-base">
               <a
